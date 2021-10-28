1837c38feb8440ca5ba196f46796738275a639ab
Klaudiusz <klaudiusz.boblack@gmail.com>
2021-10-28 13:37:49 +0200

    Check is null parameter

--- src/main/java/com/example/demo/SpringController.java
+++ src/main/java/com/example/demo/SpringController.java
@@ -12,8 +12,15 @@ public class SpringController {
 
     @RequestMapping(value = "/checkString", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public HashMap<String, Object> checkString(@RequestParam("stringToCheck") String stringToCheck) {
-        Analysis analysis =  new Analysis(stringToCheck);
+
+
         HashMap<String, Object> map = new HashMap<>();
+        if(stringToCheck==null)
+        {
+            map.put("message","Problems parsing JSON");
+            return map;
+        }
+        Analysis analysis =  new Analysis(stringToCheck);
         map.put("data",analysis.toString());
         return map;
     }
