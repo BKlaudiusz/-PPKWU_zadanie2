8dec5983d747e687dcdda05bbaf33671bd37cf6d
Klaudiusz <klaudiusz.boblack@gmail.com>
2021-10-28 13:38:00 +0200

    clean code

--- src/main/java/com/example/demo/SpringController.java
+++ src/main/java/com/example/demo/SpringController.java
@@ -13,7 +13,6 @@ public class SpringController {
     @RequestMapping(value = "/checkString", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public HashMap<String, Object> checkString(@RequestParam("stringToCheck") String stringToCheck) {
 
-
         HashMap<String, Object> map = new HashMap<>();
         if(stringToCheck==null)
         {
