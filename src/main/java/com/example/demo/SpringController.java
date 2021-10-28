97c71ab4f01459f7724da3ae64afc16bc99437f6
Klaudiusz <klaudiusz.boblack@gmail.com>
2021-10-28 13:27:55 +0200

    change SpringController

--- src/main/java/com/example/demo/SpringController.java
+++ src/main/java/com/example/demo/SpringController.java
@@ -1,15 +1,20 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 public class SpringController {
 
-    @GetMapping(value="/checkString")
-    public Analysis checkString(@RequestParam("stringToCheck") String stringToCheck){
-        Analysis analysis = new Analysis(stringToCheck);
-        return analysis;
+    @RequestMapping(value = "/checkString", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public HashMap<String, Object> checkString(@RequestParam("stringToCheck") String stringToCheck) {
+        Analysis analysis =  new Analysis(stringToCheck);
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("data",analysis.toString());
+        return map;
     }
 }
