9e62312cacaa378a4375a9207c2ca015d24ef454
Klaudiusz <klaudiusz.boblack@gmail.com>
2021-10-28 13:27:42 +0200

    add method do calculate

--- src/main/java/com/example/demo/Analysis.java
+++ src/main/java/com/example/demo/Analysis.java
@@ -4,15 +4,31 @@ public class Analysis {
     private long upperCase;
     private String stringToCheck;
     private long lowerCase;
+    private long countNumber;
 
     public Analysis(String stringToCheck) {
         this.stringToCheck = stringToCheck;
+        this.countUpperCase();
+        this.countLowerCase();
+        this.countNumberCase();
     }
-    private void countUpperCase(String inputString) {
-        upperCase = inputString.chars().filter((s)->Character.isUpperCase(s)).count();
+    private void countUpperCase() {
+        upperCase = stringToCheck.chars().filter((s)->Character.isUpperCase(s)).count();
+    }
+    private  void  countLowerCase() {
+        lowerCase = stringToCheck.chars().filter((s)->Character.isLowerCase(s)).count();
+    }
+    private  void  countNumberCase() {
+        countNumber = stringToCheck.chars().filter((s)->Character.isDigit(s)).count();
     }
 
-    private  void  countLowerCase(String inputString) {
-        lowerCase = inputString.chars().filter((s)->Character.isLowerCase(s)).count();
+    @Override
+    public String toString() {
+        return "Analysis{" +
+                "upperCase=" + upperCase +
+                ", stringToCheck='" + stringToCheck + '\'' +
+                ", lowerCase=" + lowerCase +
+                ", countNumber=" + countNumber +
+                '}';
     }
 }
