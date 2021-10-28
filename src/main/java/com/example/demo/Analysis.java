f1b983e03ad7ebaf44986443b75fd491652feecf
Klaudiusz <klaudiusz.boblack@gmail.com>
2021-10-28 12:54:49 +0200

    create method to calculate upper and lower case

--- src/main/java/com/example/demo/Analysis.java
+++ src/main/java/com/example/demo/Analysis.java
@@ -1,7 +1,18 @@
 package com.example.demo;
 
 public class Analysis {
-    public Analysis(String stringToCheck) {
+    private long upperCase;
+    private String stringToCheck;
+    private long lowerCase;
 
+    public Analysis(String stringToCheck) {
+        this.stringToCheck = stringToCheck;
+    }
+    private void countUpperCase(String inputString) {
+        upperCase = inputString.chars().filter((s)->Character.isUpperCase(s)).count();
+    }
+
+    private  void  countLowerCase(String inputString) {
+        lowerCase = inputString.chars().filter((s)->Character.isLowerCase(s)).count();
     }
 }
